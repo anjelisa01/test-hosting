b87ac3549f382c543611abfd9883a7dd3c547a3d
Anjelisa <anjelisasouisa2001@gmail.com>
2023-08-18 19:08:10 +0700

    initial commit

--- testHosting.py
+++ testHosting.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
-import mysql.connector
+import pymysql
 def getDataSendiri1():
     mycursor.execute('SELECT tb_jenis_var.nama_var ,tb_dataset.bln_tahun,tb_dataset.nilai,tb_dataset.id_admin FROM tb_dataset INNER JOIN tb_jenis_var ON tb_dataset.id_jenis_var=tb_jenis_var.id_jenis_var where tb_dataset.id_jenis_var=1;')
     data = mycursor.fetchall()
     return data
 
-database_connection = mysql.connector.connect(
+database_connection = pymysql.connect(
    host="boon1y6eurxayeoke2kx-mysql.services.clever-cloud.com",
    port=3306,
    user="uco7itezylzntgza",
